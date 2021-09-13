48f72952f04fb431f3e1fc99e94a6987083246ca
Jaemin-K <rlawoals6281@naver.com>
2021-09-13 21:53:32 +0900

    complete project

--- app/src/main/java/com/example/calculator/MainActivity.java
+++ app/src/main/java/com/example/calculator/MainActivity.java
@@ -6,11 +6,13 @@ import android.os.Bundle;
 import android.view.View;
 import android.widget.Button;
 import android.widget.TextView;
+import android.widget.Toast;
 
 import java.util.Stack;
 
 public class MainActivity extends AppCompatActivity {
 
+    Button btn_0;
     Button btn_1;
     Button btn_2;
     Button btn_3;
@@ -25,17 +27,22 @@ public class MainActivity extends AppCompatActivity {
     Button btn_div;
     Button btn_mul;
     Button btn_result;
+    Button btn_clear;
     TextView tv_insert;
     String insert_num = null;
+    String insert_arr[] = new String[20];
+    String postfix_arr[] = new String[20];
+
     Stack <String> infix_stack = new Stack<>();
     Stack <String> postfix_stack = new Stack<>();
-
+    int count = 0;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
 
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        btn_0 = findViewById(R.id.btn_0);
         btn_1 = findViewById(R.id.btn_1);
         btn_2 = findViewById(R.id.btn_2);
         btn_3 = findViewById(R.id.btn_3);
@@ -50,7 +57,20 @@ public class MainActivity extends AppCompatActivity {
         btn_div = findViewById(R.id.btn_div);
         btn_mul = findViewById(R.id.btn_mul);
         btn_result = findViewById(R.id.btn_result);
+        btn_clear = findViewById(R.id.btn_clear);
         tv_insert = findViewById(R.id.tv_insert);
+
+        btn_0.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                if(insert_num == null){
+                    insert_num = "0";
+                    tv_insert.setText(insert_num);
+                }
+                else
+                    insert_num = insert_num + "0";tv_insert.setText(insert_num);
+            }
+        });
 
         btn_1.setOnClickListener(new View.OnClickListener() {
             @Override
@@ -163,18 +183,8 @@ public class MainActivity extends AppCompatActivity {
         btn_plus.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
-                infix_stack.push(tv_insert.getText().toString());
-                infix_stack.push("+");
-                tv_insert.setText(null);
-                insert_num = null;
-            }
-        });
-
-        btn_plus.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View view) {
-                infix_stack.push(tv_insert.getText().toString());
-                infix_stack.push("+");
+                insert_arr[count++] = tv_insert.getText().toString();
+                insert_arr[count++] = "+";
                 tv_insert.setText(null);
                 insert_num = null;
             }
@@ -183,8 +193,8 @@ public class MainActivity extends AppCompatActivity {
         btn_minus.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
-                infix_stack.push(tv_insert.getText().toString());
-                infix_stack.push("-");
+                insert_arr[count++] = tv_insert.getText().toString();
+                insert_arr[count++] = "-";
                 tv_insert.setText(null);
                 insert_num = null;
             }
@@ -193,8 +203,8 @@ public class MainActivity extends AppCompatActivity {
         btn_div.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
-                infix_stack.push(tv_insert.getText().toString());
-                infix_stack.push("/");
+                insert_arr[count++] = tv_insert.getText().toString();
+                insert_arr[count++] = "/";
                 tv_insert.setText(null);
                 insert_num = null;
             }
@@ -203,12 +213,125 @@ public class MainActivity extends AppCompatActivity {
         btn_mul.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
-                infix_stack.push(tv_insert.getText().toString());
-                infix_stack.push("*");
+                insert_arr[count++] = tv_insert.getText().toString();
+                insert_arr[count++] = "*";
                 tv_insert.setText(null);
                 insert_num = null;
             }
         });
 
+        btn_result.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                if(count == 0){
+                    Toast.makeText(getApplicationContext(), "입력값이 비었습니다", Toast.LENGTH_SHORT).show();
+                }
+
+                else{
+                    insert_arr[count] = tv_insert.getText().toString();
+
+                    for(int i = 0 ; i<insert_arr.length; i++){
+                        if(insert_arr[i] == null && insert_arr[i-1]!=null){
+                            postfix_stack.push(infix_stack.pop());
+                            break;
+                        }
+                        else{
+                            int priority = prec(insert_arr[i]);
+                            switch (insert_arr[i]){
+                                case "+" :
+                                case "-" :
+                                case "*" :
+                                case "/" :
+                                    while(!infix_stack.isEmpty() && prec(infix_stack.peek()) <= priority){
+                                        postfix_stack.push(infix_stack.pop());
+                                    }
+                                    infix_stack.push(insert_arr[i]);
+                                    break;
+                                default:
+                                    postfix_stack.push(insert_arr[i]);
+
+                            }
+
+
+                        }
+                    }
+
+                    //후위표현식 계산
+                    int size = postfix_stack.size();
+
+                    for(int i = 0 ; i<postfix_arr.length; i++){
+                        if(postfix_stack.empty())
+                            break;
+                        else
+                        postfix_arr[i] = postfix_stack.pop();
+                    };
+
+                    int a=0, b = 0;
+
+                    for(int i = size-1 ; i>=0; i--){
+                        if(postfix_arr[i] != "+" && postfix_arr[i] != "-" && postfix_arr[i] != "*" && postfix_arr[i] != "/"){
+                            postfix_stack.push(postfix_arr[i]);
+                        }
+                        else{
+                            b = Integer.parseInt(postfix_stack.pop());
+                            a = Integer.parseInt(postfix_stack.pop());
+
+                            switch (postfix_arr[i]){
+                                case "+":
+                                    postfix_stack.push(Integer.toString(a+b));
+                                    break;
+                                case "-":
+                                    postfix_stack.push(Integer.toString(a-b));
+                                    break;
+                                case "*":
+                                    postfix_stack.push(Integer.toString(a*b));
+                                    break;
+                                case "/":
+                                    postfix_stack.push(Integer.toString(a/b));
+                                    break;
+                            }
+
+                        }
+                    }
+
+                    tv_insert.setText(postfix_stack.pop());
+
+                }
+
+
+
+            }
+        });
+
+        btn_clear.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                insert_num = null;
+                tv_insert.setText(null);
+                infix_stack.clear();
+                postfix_stack.clear();
+                count=0;
+                for(int i = 0 ; i<insert_arr.length; i++){
+                    insert_arr[i] = null;
+                    postfix_arr[i] = null;
+                }
+
+            }
+        });
+
+    }
+
+    int prec(String s){
+        switch(s){
+            case "+":
+            case "-" :
+                return 1;
+            case "*":
+            case "/":
+                return 2;
+            default:
+                return 0;
+
+        }
     }
 }
